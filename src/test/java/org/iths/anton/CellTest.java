3739fd7e88c18ae349feaa1f66e6ac9594d45a36
Anton Johansson <anton.elis@hotmail.com>
2020-10-22 15:29:07 +0200

    Created Tests where cell should die with 5 - 8(MAX) neighbours

--- src/test/java/org/iths/anton/CellTest.java
+++ src/test/java/org/iths/anton/CellTest.java
@@ -36,16 +36,39 @@ class CellTest {
         CellState actual = cell.GetNextState(2);
         assertEquals(CellState.ALIVE, actual);
     }
+
     @Test
     public void cellShouldLiveWithThreeNeighbours(){
         Cell cell = new Cell(CellState.ALIVE);
         CellState actual = cell.GetNextState(3);
         assertEquals(CellState.ALIVE, actual);
     }
+
     @Test
-    public void cellShouldDieWithFourNeighbours(){
+    public void cellShouldDieWithFiveNeighbours(){
         Cell cell = new Cell(CellState.ALIVE);
-        CellState actual = cell.GetNextState(4);
+        CellState actual = cell.GetNextState(5);
+        assertEquals(CellState.DEAD, actual);
+    }
+
+    @Test
+    public void cellShouldDieWithSixNeighbours(){
+        Cell cell = new Cell(CellState.ALIVE);
+        CellState actual = cell.GetNextState(6);
+        assertEquals(CellState.DEAD, actual);
+    }
+
+    @Test
+    public void cellShouldDieWithSevenNeighbours(){
+        Cell cell = new Cell(CellState.ALIVE);
+        CellState actual = cell.GetNextState(7);
+        assertEquals(CellState.DEAD, actual);
+    }
+
+    @Test
+    public void cellShouldDieWithEightNeighbours(){
+        Cell cell = new Cell(CellState.ALIVE);
+        CellState actual = cell.GetNextState(8);
         assertEquals(CellState.DEAD, actual);
     }
 }
